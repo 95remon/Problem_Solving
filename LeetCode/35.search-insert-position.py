8687f55349f147b78ad4e659bbecd8d96cc0f380
remon <95remon@gmail.com>
2024-04-26 20:15:11 +0300

    [35] Search Insert Position (binary search)

--- LeetCode/35.search-insert-position.py
+++ LeetCode/35.search-insert-position.py
@@ -7,13 +7,30 @@
 # @lc code=start
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
-        n = len(nums)
-        res=0
-        for i in range(n):
-            if nums[i]==target:
-                res=i
-            elif nums[i]<target:
-                res=i+1
-        return res
+        l = 0 
+        r = len(nums) - 1
+        
+        while l <= r :
+            m = (l + r) // 2
+            if nums[m] > target :
+                r = m - 1
+            elif nums[m] < target :
+                l = m + 1
+            else :
+                return m
+            
+        return l
+
+        
+        
+        
+        # n = len(nums)
+        # res=0
+        # for i in range(n):
+        #     if nums[i]==target:
+        #         res=i
+        #     elif nums[i]<target:
+        #         res=i+1
+        # return res
 # @lc code=end
 
